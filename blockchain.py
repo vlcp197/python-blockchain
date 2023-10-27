1a0593776867a706157685699343a19f26d55c89
vlcp197 <vlcp197@gmail.com>
2023-10-27 00:14:06 -0300

    Create Blockchain class and some primitive methods

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+class Blockchain():
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    # Creates a new block and adds it to the chain
+    def new_block(self):
+        return
+
+    # Adds a new transaction to the list of transactions
+    def new_transaction(self):
+        return
+
+    # Hashes a block
+    def hash_block(block):
+        return
+
+    # Return the last block in the chain
+    def last_block(self):
+        return
